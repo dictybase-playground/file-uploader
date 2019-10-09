b5ddc7961956ba22c8147f702f82a5b4432f6183
wildlifehexagon <crossthebreeze@gmail.com>
2019-10-09 12:43:09 -0500

    refactor: keep upload logic in same function

--- internal/app/upload/minio.go
+++ internal/app/upload/minio.go
@@ -1,24 +1,55 @@
 package upload
 
 import (
+	"io/ioutil"
+
 	log "github.com/dictybase-playground/file-uploader/internal/logger"
-	m "github.com/dictybase-playground/file-uploader/internal/upload/minio"
 	"github.com/minio/minio-go/v6"
 	"github.com/urfave/cli"
 )
 
 func UploadFilesMinio(c *cli.Context) error {
+	endpoint := c.String("minio-endpoint")
+	accessKey := c.String("minio-access-key")
+	secretKey := c.String("minio-secret-key")
+	bucket := c.String("minio-bucket")
+	folder := c.String("folder")
 	useSSL := true
+
 	logger, err := log.GetLogger(c)
 	if err != nil {
 		return cli.NewExitError(err.Error(), 2)
 	}
-	// Initialize minio client object
-	minioClient, err := minio.New(c.String("minio-endpoint"), c.String("minio-access-key"), c.String("minio-secret-key"), useSSL)
+
+	minioClient, err := minio.New(endpoint, accessKey, secretKey, useSSL)
 	if err != nil {
 		return cli.NewExitError(err.Error(), 2)
 	}
 
-	m.NewUploadCreator(c.String("minio-bucket"), c.String("folder"), minioClient, logger)
+	found, err := minioClient.BucketExists(bucket)
+	if err != nil {
+		logger.Errorf("could not find bucket %s", err)
+		return cli.NewExitError(err.Error(), 2)
+	}
+	if found {
+		logger.Info("Bucket found")
+	}
+
+	files, err := ioutil.ReadDir(folder)
+	if err != nil {
+		logger.Errorf("couldn't read directory %s", err)
+		return cli.NewExitError(err.Error(), 2)
+	}
+
+	for _, file := range files {
+		filePath := folder + "/" + file.Name()
+		n, err := minioClient.FPutObject(bucket, file.Name(), filePath, minio.PutObjectOptions{})
+		if err != nil {
+			logger.Errorf("couldn't upload file %s", err)
+			return cli.NewExitError(err.Error(), 2)
+		}
+		logger.Infof("Successfully uploaded %s of size %d\n", file.Name(), n)
+	}
+
 	return nil
 }
--- internal/upload/upload.go
+++
@@ -1,7 +0,0 @@
-package upload
-
-// Uploader is an interface for uploading files
-type Uploader interface {
-	// UploadFolder uploads files inside a folder to Minio
-	UploadFolder() error
-}
